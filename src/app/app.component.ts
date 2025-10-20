5dfa11be6699e2327e69c6404eb330dd8cad60b5
suffle-girl <sarka.flidrova@email.cz>
2025-10-20 18:49:12 +0200

    CalendarComp.: Create new calendar component and add it to the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
